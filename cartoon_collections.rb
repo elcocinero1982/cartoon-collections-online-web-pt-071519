310bbefc74ecddd8c0a73096f12013de1d64fb48
jose luis <cortezluisjose@gmail.com>
2019-07-27 04:52:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)# code an argument here
  array = ["Doc", "Dopey", "Bashful", "Grumpy"] # Your code puts "array"
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
 end
 
